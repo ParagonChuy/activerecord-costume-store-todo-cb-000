1e68ff9cffc26aa04c6a28222ba2af4ab00c583c
Jesus Vega <chuyspc@gmail.com>
2017-02-07 09:45:31 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.float :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
+      t.timestamps
     end
   end
 end
